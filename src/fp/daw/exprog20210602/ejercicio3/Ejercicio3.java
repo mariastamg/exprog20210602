06f7b52bc5ff075a4b6fa9a993135ced28eef87c
alumno <alumno@informatica.cifpaviles.pa>
2021-06-02 19:24:14 +0200

    Examen finalizado

--- src/fp/daw/exprog20210602/ejercicio3/Ejercicio3.java
+++ src/fp/daw/exprog20210602/ejercicio3/Ejercicio3.java
@@ -5,12 +5,25 @@ import java.util.Queue;
 
 public class Ejercicio3 {
 	static Queue<Integer> colaNumeros = new LinkedList<Integer>();
+	//Cola aux para guardar los n numeros de la colaNumeros y poder invertirlos
+	static Queue<Integer> colaAuxiliar = new LinkedList<Integer>();
+	//La cola resultado
+	static Queue<Integer> colaFinal=new LinkedList<Integer>();
 
 	public static void main(String[] args) {
 
 	}
 
 	public static void invertir(int n, Queue<Integer> colaNumeros) {
+		
+		if(!colaNumeros.isEmpty()) {
+			for(int i=0;i<n;i++) {
+				int numeroSacado=colaNumeros.poll();
+				colaAuxiliar.offer(numeroSacado);
+			
+			}
+		}
+		
 
 	}
 }
